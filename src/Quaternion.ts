fd58bbbafddad254d1046a38ac5e030ab4a698c3
sueuegunn <sueuegunn@gmail.com>
2025-12-22 22:05:00 +0900

    add Quaternion docs

--- src/Quaternion.ts
+++ src/Quaternion.ts
@@ -1,19 +1,32 @@
 import type { Additive } from "./Additive";
 import type { Clonable } from "./Clonable";
-import type { Matrix4 } from "./Matrix4";
 import type { Multiplicative } from "./Multiplicative";
 import type { Scalable } from "./Scalable";
 import { Vector3 } from "./Vector3";
 
+/**
+ * Represents a quaternion using Hamilton's notation: q = a + bi + cj + dk
+ */
 class Quaternion implements Clonable<Quaternion>, Additive<Quaternion>, Multiplicative<Quaternion>, Scalable<Quaternion> {
   private _a: number;
   private _b: number;
   private _c: number;
   private _d: number;
 
-  /** @ignore */
-  static temporary = Quaternion.identity();
+  private static temporary = Quaternion.identity();
 
+   /**
+   * Constructor of quaternion: q = a + bi + cj + dk
+   * @param a the first real component
+   * @param b the i-coefficient
+   * @param c the j-coefficient
+   * @param d the k-coefficient
+   * 
+   * @example
+   * ```ts
+   * const q = new Quaternion(1, 0, 0, 0);
+   * ```
+   */
   private constructor(a: number, b: number, c: number, d: number) {
     this._a = a;
     this._b = b;
@@ -21,23 +34,67 @@ class Quaternion implements Clonable<Quaternion>, Additive<Quaternion>, Multipli
     this._d = d;
   }
 
+  /**
+   * Gets the first real component of this quaternion
+   * @readonly
+   * 
+   * @example
+   * ```ts
+   * const q = new Quaternion(1, 2, 3, 4);
+   * console.log(q.a); // 1
+   * ```
+   */
   get a(): number {
     return this._a;
   }
 
+   /**
+   * Gets the i-coefficient of this quaternion
+   * @readonly
+   * 
+   * @example
+   * ```ts
+   * const q = new Quaternion(1, 2, 3, 4);
+   * console.log(q.b); // 2
+   * ```
+   */
   get b(): number {
     return this._b;
   }
 
+   /**
+   * Gets the j-coefficient of this quaternion
+   * @readonly
+   * 
+   * @example
+   * ```ts
+   * const q = new Quaternion(1, 2, 3, 4);
+   * console.log(q.c); // 3
+   * ```
+   */
   get c(): number {
     return this._c;
   }
 
+  /**
+   * Gets the k-coefficient of this quaternion
+   * @readonly
+   * 
+   * @example
+   * ```ts
+   * const q = new Quaternion(1, 2, 3, 4);
+   * console.log(q.d); // 4
+   * ```
+   */
   get d(): number {
     return this._d;
   }
 
   /**
+   * @returns new identity quaternion instance
+   * 
+   * @group Factory Methods
+   * 
    * @example
    * ```ts
    * const q = Quaternion.identity();
@@ -48,17 +105,57 @@ class Quaternion implements Clonable<Quaternion>, Additive<Quaternion>, Multipli
     return new Quaternion(1, 0, 0, 0);
   }
 
+  /**
+   * @param axis the rotation axis
+   * @param radian the rotation angle in radians
+   * @returns new normalized quaternion instance from axis and radian
+   * 
+   * @group Factory Methods
+   * 
+   * @example
+   * ```ts
+   * const axis = new Vector3(0, 1, 0);
+   * const radian = Math.PI / 3;
+   * const q = Quaternion.identity(axis, radian);
+   * ```
+   */
   static fromAxisAndRadian(axis: Vector3, radian: number): Quaternion {
     const quaternion = Quaternion.identity();
     quaternion.setAxisAndRadian(axis, radian);
     return quaternion;
   }
 
+  /**
+   * @returns new cloned quaternion instance
+   * 
+   * @example
+   * ```ts
+   * const q = Quaternion.identity();
+   * const c = q.clone();
+   * console.log(c); // (1, 0, 0, 0)
+   * ```
+   */
   clone(): Quaternion {
     const {a, b, c, d} = this;
     return new Quaternion(a, b, c, d);
   }
 
+  /**
+   * Sets all components of this quaternion: q = a + bi + cj + dk (mutates this)
+   * @param a the first real component
+   * @param b the i-coefficient
+   * @param c the j-coefficient
+   * @param d the k-coefficient
+   * @returns this instance, for method chaining
+   * 
+   * @example
+   * ```ts
+   * const q = Quaternion.identity();
+   * console.log(q); // (1, 0, 0, 0)
+   * q.setValues(0, 0, 0, 1);
+   * console.log(q); // (0, 0, 0, 1)
+   * ```
+   */
   setValues(a: number, b: number, c: number, d: number): Quaternion {
     this._a = a;
     this._b = b;
@@ -67,14 +164,50 @@ class Quaternion implements Clonable<Quaternion>, Additive<Quaternion>, Multipli
     return this;
   }
 
+  /**
+   * Sets this quaternion to other quaternion (mutates this)
+   * @param other the other quaternion
+   * @returns this instance, for method chaining
+   * 
+   * @example
+   * ```ts
+   * 
+   * ```
+   */
   set(other: Quaternion): Quaternion {
     const {a, b, c, d} = other;
     return this.setValues(a, b, c, d);
   }
 
+  /**
+   * Sets this instance to identity quaternion (mutates this)
+   * @returns this instance, for method chaining
+   * 
+   * @example
+   * ```ts
+   * const q = new Quaternion(1, 2, 3, 4);
+   * q.setIdentity();
+   * console.log(q); // (1, 0, 0, 0)
+   * ```
+   */
   setIdentity(): void {
     this.setValues(1, 0, 0, 0);
   }
+
+  /**
+   * Sets this quaternion from axis and radian (mutates this)
+   * @param axis the rotation axis
+   * @param radian the rotation angle in radians
+   * @returns this instance, for method chaining
+   * 
+   * @example
+   * ```ts
+   * const q = Quaternion.identity();
+   * const axis = new Vector3(0, 1, 0);
+   * const radian = Math.PI / 3;
+   * q.setAxisAndRadian(axis, radian);
+   * ```
+   */
 
   setAxisAndRadian(axis: Vector3, radian: number): void {
     if (axis.isZero()) {
@@ -87,15 +220,46 @@ class Quaternion implements Clonable<Quaternion>, Additive<Quaternion>, Multipli
     this.setValues(Math.cos(radian / 2), x * sin, y * sin, z * sin);
   }
 
+  /**
+   * Calculates squared norm of this quaternion (pure)
+   * @returns squared norm of this quaternion
+   * 
+   * @example
+   * ```ts
+   * const q = new Quaternion(1, 2, 2, 4);
+   * console.log(q.squaredNorm()); // 25
+   * ```
+   */
   squaredNorm(): number {
     const {a, b, c, d} = this;
     return a ** 2 + b ** 2 + c ** 2 + d ** 2;
   }
 
+  /**
+   * Calculates norm of this quaternion (pure)
+   * @returns norm of this quaternion
+   * 
+   * @example
+   * ```ts
+   * const q = new Quaternion(1, 2, 2, 4);
+   * console.log(q.norm()); // 5
+   * ```
+   */
   norm(): number {
     return Math.sqrt(this.squaredNorm());
   }
 
+  /**
+   * Calculates conjugate of this quaternion (mutates this)
+   * @returns this instance, for method chaining
+   * 
+   * @example
+   * ```ts
+   * const q = new Quaternion(1, 2, 3, 4);
+   * q.conjugate();
+   * console.log(q); // (1, -2, -3, -4)
+   * ```
+   */
   conjugate(): Quaternion {
     this._b *= -1;
     this._c *= -1;
@@ -103,43 +267,36 @@ class Quaternion implements Clonable<Quaternion>, Additive<Quaternion>, Multipli
     return this;
   }
 
+  /**
+   * Calculates inverse of this quaternion (mutates this)
+   * @returns this instance, for method chaining
+   * 
+   * @example
+   * ```ts
+   * const q = new Quaternion(1, 2, 3, 4);
+   * q.inverse();
+   * console.log(q); // (0.0333, -0.0667, -0.1, -0.1333)
+   * ```
+   */
   inverse(): Quaternion {
     const norm2 = this.squaredNorm();
     return this.conjugate().scalarDivide(norm2);
   }
 
-  setToMatrix4(matrix: Matrix4): void {
-    const s = 2 / this.squaredNorm();
-    const {a, b, c, d} = this;
-    const b2 = b ** 2;
-    const c2 = c ** 2;
-    const d2 = d ** 2;
-    const ab = a * b;
-    const ac = a * c;
-    const ad = a * d;
-    const bc = b * c;
-    const bd = b * d;
-    const cd = c * d;
-    matrix.setValues(
-      1 - s * (c2 + d2),
-      s * (bc - ad),
-      s * (bd + ac),
-      0,
-      s * (bc * ad),
-      1 - s * (b2 + d2),
-      s * (cd - ab),
-      0,
-      s * (bd - ac),
-      s * (cd + ab),
-      1 - s * (b2 + c2),
-      0,
-      0,
-      0,
-      0,
-      1,
-    );
-  }
-
+  /**
+   * Adds other quaternion to this instance (mutates this)
+   * @param other other quaternion
+   * @returns this instance, for method chaining
+   * 
+   * @example
+   * ```ts
+   * const q1 = new Quaternion(1, 2, 3, 4);
+   * const q2 = new Quaternion(5, 6, 7, 8);
+   * q1.add(q2);
+   * console.log(q1); // (6, 8, 10, 12)
+   * console.log(q2); // (5, 6, 7, 8)
+   * ```
+   */
   add(other: Quaternion): Quaternion {
     const {a, b, c, d} = other;
     this._a += a;
@@ -149,6 +306,20 @@ class Quaternion implements Clonable<Quaternion>, Additive<Quaternion>, Multipli
     return this;
   }
 
+  /**
+   * Subtracts other quaternion from this instance (mutates this)
+   * @param other other quaternion
+   * @returns this instance, for method chaining
+   * 
+   * @example
+   * ```ts
+   * const q1 = new Quaternion(5, 6, 7, 8);
+   * const q2 = new Quaternion(1, 2, 3, 4);
+   * q1.subtract(q2);
+   * console.log(q1); // (4, 4, 4, 4)
+   * console.log(q2); // (1, 2, 3, 4)
+   * ```
+   */
   subtract(other: Quaternion): Quaternion {
     const {a, b, c, d} = other;
     this._a -= a;
@@ -158,6 +329,20 @@ class Quaternion implements Clonable<Quaternion>, Additive<Quaternion>, Multipli
     return this;
   }
 
+  /**
+   * Multiplies this instance by other quaternion (mutates this)
+   * @param other other quaternion
+   * @returns this instance, for method chaining
+   * 
+   * @example
+   * ```ts
+   * const q1 = new Quaternion(1, 2, 3, 4);
+   * const q2 = new Quaternion(5, 6, 7, 8);
+   * q1.multiply(q2);
+   * console.log(q1); // (-60, 12, 30, 24)
+   * console.log(q2); // (5, 6, 7, 8)
+   * ```
+   */
   multiply(other: Quaternion): Quaternion {
     const {a: ta, b: tb, c: tc, d: td} = this;
     const {a: oa, b: ob, c: oc, d: od} = other;
@@ -168,12 +353,38 @@ class Quaternion implements Clonable<Quaternion>, Additive<Quaternion>, Multipli
     return this;
   }
 
+  /**
+   * Divides this instance by other quaternion (mutates this)
+   * @param other other quaternion
+   * @returns this instance, for method chaining
+   * 
+   * @example
+   * ```ts
+   * const q1 = new Quaternion(1, 2, 3, 4);
+   * const q2 = new Quaternion(5, 6, 7, 8);
+   * q1.divide(q2);
+   * console.log(q1); // (0.1333, 0.0667, 0.1, 0.1333)
+   * console.log(q2); // (5, 6, 7, 8)
+   * ```
+   */
   divide(other: Quaternion): Quaternion {
     const {temporary} = Quaternion;
     temporary.set(other).inverse();
     return this.multiply(temporary);
   }
 
+  /**
+   * Multiplies this quaternion by a scalar (mutates this)
+   * @param scalar scalar value
+   * @returns this instance, for method chaining
+   * 
+   * @example
+   * ```ts
+   * const q = new Quaternion(1, 2, 3, 4);
+   * q.scalarMultiply(2);
+   * console.log(q); // (2, 4, 6, 8)
+   * ```
+   */
   scalarMultiply(scalar: number): Quaternion {
     this._a *= scalar;
     this._b *= scalar;
@@ -182,6 +393,18 @@ class Quaternion implements Clonable<Quaternion>, Additive<Quaternion>, Multipli
     return this;
   }
 
+  /**
+   * Divides this quaternion by a scalar (mutates this)
+   * @param scalar scalar value
+   * @returns this instance, for method chaining
+   * 
+   * @example
+   * ```ts
+   * const q = new Quaternion(2, 4, 6, 8);
+   * q.scalarDivide(2);
+   * console.log(q); // (1, 2, 3, 4)
+   * ```
+   */
   scalarDivide(scalar: number): Quaternion {
     this._a /= scalar;
     this._b /= scalar;
@@ -190,6 +413,18 @@ class Quaternion implements Clonable<Quaternion>, Additive<Quaternion>, Multipli
     return this;
   }
 
+  /**
+   * Rotates this quaternion around X axis by given radian (mutates this)
+   * @param radian the rotation angle in radians
+   * @returns this instance, for method chaining
+   * 
+   * @example
+   * ```ts
+   * const q = Quaternion.identity();
+   * q.rotateX(Math.PI / 2);
+   * console.log(q); // (0.7071, 0.7071, 0, 0)
+   * ```
+   */
   rotateX(radian: number): Quaternion {
     const {temporary} = Quaternion;
     temporary.setIdentity();
@@ -197,6 +432,18 @@ class Quaternion implements Clonable<Quaternion>, Additive<Quaternion>, Multipli
     return this.multiply(temporary);
   }
 
+  /**
+   * Rotates this quaternion around Y axis by given radian (mutates this)
+   * @param radian the rotation angle in radians
+   * @returns this instance, for method chaining
+   * 
+   * @example
+   * ```ts
+   * const q = Quaternion.identity();
+   * q.rotateY(Math.PI / 2);
+   * console.log(q); // (0.7071, 0, 0.7071, 0)
+   * ```
+   */
   rotateY(radian: number): Quaternion {
     const {temporary} = Quaternion;
     temporary.setIdentity();
@@ -204,6 +451,18 @@ class Quaternion implements Clonable<Quaternion>, Additive<Quaternion>, Multipli
     return this.multiply(temporary);
   }
 
+  /**
+   * Rotates this quaternion around Z axis by given radian (mutates this)
+   * @param radian the rotation angle in radians 
+   * @returns this instance, for method chaining
+   * 
+   * @example
+   * ```ts
+   * const q = Quaternion.identity();
+   * q.rotateZ(Math.PI / 2);
+   * console.log(q); // (0.7071, 0, 0, 0.7071)
+   * ```
+   */
   rotateZ(radian: number): Quaternion {
     const {temporary} = Quaternion;
     temporary.setIdentity();
