b697dce3d6490d1d838fc755d19124f5c286f54b
sueuegunn <sueuegunn@gmail.com>
2025-12-26 22:47:55 +0900

    upload test coverage summary to gist

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ['json-summary', 'html'],
+      reporter: ['json-summary', 'text'],
     },
   },
 });
